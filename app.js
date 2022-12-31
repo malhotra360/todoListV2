cbf9486298fc4bc37ec1866a890a4ef62abc5c38
malhotra360 <53490507+malhotra360@users.noreply.github.com>
2022-12-31 04:01:21 -0600

    Initial commit

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const _ = require("lodash");
 
 const app = express();
 
+
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
@@ -14,6 +15,7 @@ app.use(express.static("public"));
 
 mongoose.set("strictQuery", false);
 mongoose.connect("mongodb+srv://ruknA:google13@cluster0.ubqvvtr.mongodb.net/todolistDB", {useNewUrlParser: true});
+
 
 const itemsSchema = {
   name: String
@@ -137,6 +139,25 @@ app.get("/about", function(req, res){
   res.render("about");
 });
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+
+          //Routes go here
+          app.all('*', (req,res) => {
+          res.json({"every thing":"is awesome"})
+          })
+
+          //Connect to the database before listening
+          connectDB().then(() => {
+          app.listen(PORT, () => {
+          console.log("listening for requests");
+          })
+          })
+                let port = process.env.PORT;
+                if (port == null || port == "") {
+                  port = 3000;
+                }
+
+
+
+app.listen(port, function() {
+  console.log("Server started on port 3000 successfully");
 });
