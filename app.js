071a9f8ea96f23741d37255a202588116da8578e
malhotra360 <53490507+malhotra360@users.noreply.github.com>
2022-12-31 04:19:04 -0600

    Made changes to server 	modified:   app.js

--- app.js
+++ app.js
@@ -146,7 +146,10 @@ app.get("/about", function(req, res){
                 }
 
 
-
+                //Routes go here
+                app.all('*', (req,res) => {
+                    res.json({"every thing":"is awesome"})
+                })
 app.listen(port, function() {
   console.log("Server started on port 3000 successfully");
 });
